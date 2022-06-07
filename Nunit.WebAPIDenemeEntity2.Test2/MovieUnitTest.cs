249a4b9f6ea354906d240358e4ec6d0c90d847b8
eren torun <beneyim@gmail.com>
2022-06-07 21:46:12 +0300

    namespace's imported

--- Nunit.WebAPIDenemeEntity2.Test2/MovieUnitTest.cs
+++ Nunit.WebAPIDenemeEntity2.Test2/MovieUnitTest.cs
@@ -1,4 +1,14 @@
-﻿namespace Nunit.WebAPIDenemeEntity2.Test
+﻿using NUnit.Framework;
+using WebAPIDenemeEntity2.Controllers;
+using WebAPIDenemeEntity2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace Nunit.WebAPIDenemeEntity2.Test
 {
     public class MovieUnitTest
     {
