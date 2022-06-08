519a667183a5a3bdfed5d714b4cb782db98f7dde
eren torun <beneyim@gmail.com>
2022-06-08 04:02:27 +0300

    UsersUnitTest's Setup method was created

--- Nunit.WebAPIDenemeEntity2.Test2/UsersUnitTest.cs
+++ Nunit.WebAPIDenemeEntity2.Test2/UsersUnitTest.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NUnit.Framework;
+using WebAPIDenemeEntity2.Controllers;
+using WebAPIDenemeEntity2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Nunit.WebAPIDenemeEntity2.Test2
+namespace Nunit.WebAPIDenemeEntity2.Test
 {
-    internal class UsersUnitTest
+    public class UsersUnitTest
     {
+        private MovieDBContext _context;
+        private UsersController _usersController;
+        private IOptions<JWTSettings> _jwtsettingsIO;
+        private JWTSettings _jwtsettings;
+        private string AccessToken;
 
+        [SetUp]
+        public void Setup()
+        {
+            var builder = WebApplication.CreateBuilder();
+            var connectionString = builder.Configuration.GetConnectionString("MovieSiteDBTest");
+            var dbContextOptions = new DbContextOptionsBuilder<MovieDBContext>().UseNpgsql(connectionString);
+            _context = new MovieDBContext(dbContextOptions.Options);
+            _context.Database.EnsureCreated();
+
+
+            var jwtSection = builder.Configuration.GetSection("JWTSettings");
+            builder.Services.Configure<JWTSettings>(jwtSection);
+            _jwtsettings = jwtSection.Get<JWTSettings>();
+            _jwtsettingsIO = Options.Create(_jwtsettings);
+
+            _usersController = new UsersController(_context, _jwtsettingsIO);
+        }
     }
 }
