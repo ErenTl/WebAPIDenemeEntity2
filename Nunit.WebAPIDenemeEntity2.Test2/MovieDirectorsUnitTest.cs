85fa17423509ddde58b6d0b3e942b647a6d4ffb9
eren torun <beneyim@gmail.com>
2022-06-08 14:53:29 +0300

    namespace was changed

--- Nunit.WebAPIDenemeEntity2.Test2/MovieDirectorsUnitTest.cs
+++ Nunit.WebAPIDenemeEntity2.Test2/MovieDirectorsUnitTest.cs
@@ -10,7 +10,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Nunit.WebAPIDenemeEntity2.Test2
+namespace Nunit.WebAPIDenemeEntity2.Test
 {
     public class MovieDirectorsUnitTest
     {
