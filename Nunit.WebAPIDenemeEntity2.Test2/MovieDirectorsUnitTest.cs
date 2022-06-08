91797b014b2ecb33ccae4c5919868125b7db6b14
eren torun <beneyim@gmail.com>
2022-06-08 14:41:53 +0300

    MovieDirectorsUnitTest.cs was created

---
+++ Nunit.WebAPIDenemeEntity2.Test2/MovieDirectorsUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using WebAPIDenemeEntity2.Controllers;
+using WebAPIDenemeEntity2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nunit.WebAPIDenemeEntity2.Test2
+{
+    public class MovieDirectorsUnitTest
+    {
+    }
+}
