d8021c3f0a4fab8d4ec879fdf44a9aeefc37192d
eren torun <beneyim@gmail.com>
2022-06-15 16:55:56 +0300

    NethereumTest was created on NUnit

---
+++ Nunit.WebAPIDenemeEntity2.Test2/NethereumTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+using Nethereum.HdWallet;
+using Nethereum.Contracts;
+
+
+using NUnit.Framework;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Nunit.WebAPIDenemeEntity2.Test
+{
+    public class NethereumTest 
+    {
+        
+    }
+}
