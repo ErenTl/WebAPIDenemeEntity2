e38944293c131b1d2a1533c665ea0a3708faa8b7
eren torun <beneyim@gmail.com>
2022-06-15 17:00:02 +0300

    SetUp was added to NethereumTest.cs

--- Nunit.WebAPIDenemeEntity2.Test2/NethereumTest.cs
+++ Nunit.WebAPIDenemeEntity2.Test2/NethereumTest.cs
@@ -17,6 +17,27 @@ namespace Nunit.WebAPIDenemeEntity2.Test
 {
     public class NethereumTest 
     {
-        
+        private static string contractJsonPath;
+        private static string contractAbi;
+        private static string contractAddress;
+        private static string chainUrl;
+        private static string networkId;
+        private static Contract mrfContract;
+
+        [SetUp]
+        public void Setup()
+        {
+            contractJsonPath = (@"C:\Users\eren_\Documents\demos\temp\webentity2fortruffle-2\WebAPIDenemeEntity2\truffle\client\src\contracts\MovieRankFactory.json");
+            chainUrl = "HTTP://127.0.0.1:7545";
+            networkId = "5777";
+
+            JObject o1 = JObject.Parse(File.ReadAllText(contractJsonPath));
+            contractAbi = o1["abi"].ToString();
+            contractAddress = o1["networks"][networkId]["address"].ToString();
+
+            var web3 = new Web3(chainUrl);
+            mrfContract = web3.Eth.GetContract(contractAbi, contractAddress);
+        }
+
     }
 }
