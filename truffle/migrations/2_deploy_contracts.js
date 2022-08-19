46bcd2d2c07cc0100d921273513a8db65a22c9d0
eren torun <beneyim@gmail.com>
2022-08-19 17:25:23 +0300

    deploy_contract.js update

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,13 +1,19 @@
-var CloneFactory = artifacts.require("./CloneFactory.sol");
-//var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var CloneFactory = artifacts.require("./utils/CloneFactory.sol");
+var Initializable = artifacts.require("./utils/Initializable.sol");
 var MovieRank = artifacts.require("./MovieRank.sol");
 //var MovieRank_2 = artifacts.require("./MovieRank_2.sol");
 var MovieRankFactory = artifacts.require("./MovieRankFactory.sol");
 //var SimpleStorage_2 = artifacts.require("./SimpleStorage_2.sol");
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+var atoken = artifacts.require("./Token/atoken.sol");
+var atokenFactory = artifacts.require("./Token/atokenFactory.sol");
 
 module.exports = async function(deployer) {
   //await deployer.deploy(SimpleStorage);
   await deployer.deploy(CloneFactory);
+  //await deployer.deploy(Initializable);
   await deployer.deploy(MovieRank);
   await deployer.deploy(MovieRankFactory, MovieRank.address);
+  await deployer.deploy(atoken);
+  await deployer.deploy(atokenFactory, atoken.address);
 };
